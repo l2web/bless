ac5f1f57ee1a52a9c589cce7b28e975e05507819
l2web <110563041+l2web@users.noreply.github.com>
2023-04-14 23:43:56 -0400

    deploy
    deploy

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,13 +1,25 @@
-const path = require('path')
 module.exports = {
   stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: [
     '@storybook/addon-links',
     '@storybook/addon-essentials',
-    '@storybook/addon-interactions'
+    '@storybook/addon-interactions',
+    '@storybook/addon-a11y'
   ],
-  framework: {
-    name: '@storybook/react-vite',
-    options: {}
+  framework: '@storybook/react',
+  core: {
+    builder: '@storybook/builder-vite'
+  },
+  features: {
+    storyStoreV7: true,
+    interactionsDebugger: true
+  },
+  staticDirs: ['../public'],
+  viteFinal: (config, { configType }) => {
+    if (configType === 'PRODUCTION') {
+      config.base = '/design-sistem-bootcamp/'
+    }
+
+    return config
   }
 }
